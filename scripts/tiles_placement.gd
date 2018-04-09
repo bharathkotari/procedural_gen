ca1f1fe8ff8f537e26d65773a1122c841b5f1e08
bharath kotari <bharathkotari@gmail.com>
2018-04-09 22:39:22 +0530

    updating the terrain generation

---
+++ scripts/tiles_placement.gd
@@ -0,0 +1,64 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var map1
+var wm
+var i=0
+var j
+var prev
+var ra
+var top_tile=0
+var bottom_tile=1
+var uphill=4
+var downhill=6
+var up_joint=5
+var down_joint=7
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	map1=get_node("TileMap")
+	wm=map1.world_to_map(Vector2(0,0))
+	randomize()
+	
+	set_process(true)
+	
+func _process(delta):
+	ra=round(rand_range(0,8))
+	if(i==0):
+		map1.set_cell(i,ra,top_tile)
+		prev=ra
+		fill_ground(i,ra)
+	elif(i-1==0):
+		map1.set_cell(i,prev,top_tile)
+		fill_ground(i,prev)
+	elif(ra<prev)and(i-1>1) and (map1.get_cell(i-2,prev)==top_tile)and (prev-1>=0):
+		map1.set_cell(i,prev-1,top_tile)
+		fill_ground(i,prev-1)
+		prev=prev-1
+	elif(ra<prev)and(i-1>1)and(map1.get_cell(i-2,prev+1)==top_tile)and(prev+1<=7):
+		map1.set_cell(i,prev-1,uphill)
+		map1.set_cell(i,prev,up_joint)
+		fill_ground(i,prev)
+		prev=prev-1
+	
+	
+	elif(ra>prev)and(i-1>0) and (map1.get_cell(i-2,prev)==top_tile)and (prev+1<=7):
+		map1.set_cell(i,prev+1,top_tile)
+		prev=prev+1
+		fill_ground(i,prev)
+	elif(ra>prev)and(i-1>0)and(map1.get_cell(i-2,prev-1)==top_tile)and(prev-1>=0):
+		map1.set_cell(i,prev,downhill)
+		map1.set_cell(i,prev+1,down_joint)
+		fill_ground(i,prev+1)
+		prev=prev+1
+	else:
+		map1.set_cell(i,prev,top_tile)
+		fill_ground(i,prev)
+		
+	i=i+1
+	
+func fill_ground(r,c):
+	for m in range(c+1,10):
+		map1.set_cell(r,m,bottom_tile)
