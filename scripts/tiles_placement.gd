9cd97beb4b72259f8940ac434a52b663cc119bff
bharathkotari@gmail.com <bharathkotari@gmail.com>
2018-04-10 18:42:54 +0530

    limited the map generation , scrolling background set , added a simple player

--- scripts/tiles_placement.gd
+++ scripts/tiles_placement.gd
@@ -4,6 +4,8 @@ extends Node2D
 # var a = 2
 # var b = "textvar"
 var map1
+var player
+var cam1
 var wm
 var i=0
 var j
@@ -19,45 +21,53 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	map1=get_node("TileMap")
+	player=get_node("Sprite")
+	player.set_pos(Vector2(0,300))
 	wm=map1.world_to_map(Vector2(0,0))
 	randomize()
 	
 	set_process(true)
 	
 func _process(delta):
-	ra=round(rand_range(0,8))
-	if(i==0):
-		map1.set_cell(i,ra,top_tile)
-		prev=ra
-		fill_ground(i,ra)
-	elif(i-1==0):
-		map1.set_cell(i,prev,top_tile)
-		fill_ground(i,prev)
-	elif(ra<prev)and(i-1>1) and (map1.get_cell(i-2,prev)==top_tile)and (prev-1>=0):
-		map1.set_cell(i,prev-1,top_tile)
-		fill_ground(i,prev-1)
-		prev=prev-1
-	elif(ra<prev)and(i-1>1)and(map1.get_cell(i-2,prev+1)==top_tile)and(prev+1<=7):
-		map1.set_cell(i,prev-1,uphill)
-		map1.set_cell(i,prev,up_joint)
-		fill_ground(i,prev)
-		prev=prev-1
-	
-	
-	elif(ra>prev)and(i-1>0) and (map1.get_cell(i-2,prev)==top_tile)and (prev+1<=7):
-		map1.set_cell(i,prev+1,top_tile)
-		prev=prev+1
-		fill_ground(i,prev)
-	elif(ra>prev)and(i-1>0)and(map1.get_cell(i-2,prev-1)==top_tile)and(prev-1>=0):
-		map1.set_cell(i,prev,downhill)
-		map1.set_cell(i,prev+1,down_joint)
-		fill_ground(i,prev+1)
-		prev=prev+1
-	else:
-		map1.set_cell(i,prev,top_tile)
-		fill_ground(i,prev)
+	if (i<100):
+		ra=round(rand_range(4,8))
+		if(i==0):
+			map1.set_cell(i,ra,top_tile)
+			prev=ra
+			fill_ground(i,ra)
+		elif(i-1==0):
+			map1.set_cell(i,prev,top_tile)
+			fill_ground(i,prev)
+		elif(ra<prev)and(i-1>1) and (map1.get_cell(i-2,prev)==top_tile)and (prev-1>=4):
+			map1.set_cell(i,prev-1,top_tile)
+			fill_ground(i,prev-1)
+			prev=prev-1
+		elif(ra<prev)and(i-1>1)and(map1.get_cell(i-2,prev+1)==top_tile)and(prev+1<=7):
+			map1.set_cell(i,prev-1,uphill)
+			map1.set_cell(i,prev,up_joint)
+			fill_ground(i,prev)
+			prev=prev-1
 		
+		
+		elif(ra>prev)and(i-1>0) and (map1.get_cell(i-2,prev)==top_tile)and (prev+1<=7):
+			map1.set_cell(i,prev+1,top_tile)
+			prev=prev+1
+			fill_ground(i,prev)
+		elif(ra>prev)and(i-1>0)and(map1.get_cell(i-2,prev-1)==top_tile)and(prev-1>=4):
+			map1.set_cell(i,prev,downhill)
+			map1.set_cell(i,prev+1,down_joint)
+			fill_ground(i,prev+1)
+			prev=prev+1
+		else:
+			map1.set_cell(i,prev,top_tile)
+			fill_ground(i,prev)
+			
 	i=i+1
+	if(Input.is_action_pressed("ui_right")):
+		#print(player.get_pos())
+		player.set_global_pos(Vector2(player.get_pos().x+10,300))
+		
+	
 	
 func fill_ground(r,c):
 	for m in range(c+1,10):
